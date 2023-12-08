d75dfaac608a5a690b49b12d83932017d0b3f431
Justinmemphis <jcarter82@gmail.com>
2023-12-07 20:49:02 -0600

    add basic logic

--- 2023/day-2/day-2-part-2.py
+++ 2023/day-2/day-2-part-2.py
@@ -3,13 +3,9 @@
 # Part 2
 
 # thoughts:
-# extract all red values from game - test against red in valid game conditions, etc.
-# split line by , and ; (actually semi colons don't matter
-#
-# 1st attempt - 2681 is too high
+# just find min numbers and then multiple redMin * blueMin * greenMin and add to result
 
 import re
-import numpy as np
 
 #load input
 #myfile = open('../input/day-2-input')
@@ -17,12 +13,6 @@ myfile = open('../input/day-2-test-1')
 data = myfile.read()
 
 dataLines = data.splitlines()
-
-#valid game conditions
-validRed = 12
-validGreen = 13
-validBlue = 14
-validMaxNumber = 14
 
 #global variables
 numberRegex = r"\d+"
@@ -32,73 +22,39 @@ result = 0
 def getNumber(phrase):
     return re.findall(numberRegex, phrase)[0]
 
-def findColor(line):
-    redMax = 0
-    blueMax = 0
-    greenMax = 0
+def findMin(line):
+    redMin = 0
+    blueMin = 0
+    greenMin = 0
     for x in line:
         if 'red' in x:
             y = int(getNumber(x))
             #print('red number: ', y)
-            if y > redMax:
-                redMax = y
+            if y < redMin:
+                redMin = y
         elif 'blue' in x:
             y = int(getNumber(x))
             #print('blue number: ', y)
-            if y > blueMax:
-                blueMax = y
+            if y < blueMin:
+                blueMin = y
         elif 'green' in x:
             y = int(getNumber(x))
             #print('green number: ', y)
-            if y > greenMax:
-                greenMax = y
-    #print('redMax, blueMax, greenMax: ', redMax, blueMax, greenMax)
-    if redMax > validRed or blueMax > validBlue or greenMax > validGreen:
-        redMax = 0
-        blueMax = 0
-        greenMax = 0
-        print('local max too high')
-        return False
-    else:
-        print('valid valid game')
-        return True
+            if y < greenMin:
+                greenMin = y
+    result += redMin * blueMin * greenMin
 
 def stripColor(line):
     colors = re.split('[:,;]+',line)
     colors.pop(0) #remove the game line
     colors = [s.lstrip() for s in colors]
     print('colors: ', colors)
-    if findColor(colors):
-        return True
-    else:
-        return False
+    findMin(colors)
 
-#main function
-def possibleGames(line):
-    global result 
-    #print('line', line)
 
-    # find maximum number - if larger than 14 throw out line
-    numbers = re.findall(numberRegex, line)
-    numbers.pop(0) #remove the game line
-    numbers = np.array(numbers,dtype=int)
-    #print('numbers',numbers)
-    if (max(numbers) > validMaxNumber):
-        print('number too large')
-        return False
-    else:
-        print('valid game')
-        if stripColor(line):
-            return True
-        else:
-            return False
-
-for index, line in enumerate(dataLines):
-    realIndex = index + 1
-    print('Game: ', realIndex)
-    if possibleGames(line):
-        result += realIndex
-        print('new result: ', result)
+for line in dataLines:
+    global result
+    stripColor(line)
 
 print('final result: ',result)
 
